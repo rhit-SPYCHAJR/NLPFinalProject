95ea7e1f9736ca1c68971cb845a0636f77b8bd22
rhit-SPYCHAJR <spychajr@rose-hulman.edu>
2024-02-16 02:08:16 -0500

    rest of prompts and responses for bias testing

--- file_io_prompting.py
+++ file_io_prompting.py
@@ -6,11 +6,22 @@ huggingfacehub_api_token = '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'
 # Define the LLM
 llm = HuggingFaceHub(repo_id='tiiuae/falcon-7b-instruct', huggingfacehub_api_token=huggingfacehub_api_token)
 
-qFile = open("q_occupations.txt", encoding="utf-8", mode = "r")
-rFile = open("r_occupations.txt", "a")
+mode = 1
+
+qFile = open("q_multi_factor.txt", encoding="utf-8", mode = "r")
+rFile = open("r_multi_factor_f.txt", "a")
 question = qFile.readline()
-while question:
+line = " "
+if mode == 1:
+  while question:
+    resp = llm.predict(question)
+    rFile.write(resp + "\n")
+    question = qFile.readline()
+else:
+  while line:
+    line = qFile.readline()
+    question += line
   resp = llm.predict(question)
   rFile.write(resp + "\n")
-  question = qFile.readline()
 qFile.close()
+rFile.close()
